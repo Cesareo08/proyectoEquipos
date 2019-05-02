06bf9fa5947fa4997cd711b7b99f047ced906efa
Cesáreo <cesareo.santiaguesrgmail.com>
2019-05-02 10:26:06 +0200

    Archivo proxecto

--- conversorUnidades/src/conversorunidades/ConversorUnidades.java
+++ conversorUnidades/src/conversorunidades/ConversorUnidades.java
@@ -1,8 +1,23 @@
 package conversorunidades;
+
+import java.util.Scanner;
+
 public class ConversorUnidades {
+    static Scanner read=new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        System.out.println("Conversor de Unidades");
+        System.out.println("Introduzca la opción que quiera utilazar: ");
+        System.out.println("    1. Conversor Unidades Longitudinales");
+        System.out.println("    2. Salir");
+        int opcion=1;
+        do{
+            opcion=read.nextInt();
+            if (opcion<1 || opcion>2) {
+                System.out.println("La opción introducida es incorrecta. Por "
+                        + "favor vuelva a introducir una opción.");
+            }
+        } while(opcion<1 || opcion>2);
     }
     
 }
