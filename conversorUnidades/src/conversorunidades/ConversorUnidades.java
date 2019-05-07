caf66ae338f2bcd4af77900df964213440e2ef65
Cesáreo <cesareo.santiaguesrgmail.com>
2019-05-07 14:48:04 +0200

    todo2

--- conversorUnidades/src/conversorunidades/ConversorUnidades.java
+++ conversorUnidades/src/conversorunidades/ConversorUnidades.java
@@ -10,14 +10,33 @@ public class ConversorUnidades {
         System.out.println("Introduzca la opción que quiera utilazar: ");
         System.out.println("    1. Conversor Unidades Longitudinales");
         System.out.println("    2. Salir");
-        int opcion=1;
+        int opcion1=1;
         do{
-            opcion=read.nextInt();
-            if (opcion<1 || opcion>2) {
+            opcion1=read.nextInt();
+            if (opcion1<1 || opcion1>2) {
                 System.out.println("La opción introducida es incorrecta. Por "
                         + "favor vuelva a introducir una opción.");
             }
-        } while(opcion<1 || opcion>2);
+        } while(opcion1<1 || opcion1>2);
+        
+        if (opcion1==1) {
+            System.out.println("Opción 1. Conversor Unidades Longitudinales");
+            for (int i = 0; i < 2; i++) {
+                System.out.println("Introduzca las unidades que quiera "
+                        + "utilizar: ");
+                do{
+                    System.out.println("    Unidades: ");
+                    System.out.println("        - km (kilómetros)");
+                    System.out.println("        - hm (hectómetros)");
+                    System.out.println("        - dam (decámetros)");
+                    System.out.println("        - m (metros)");
+                    System.out.println("        - dm (decímetros)");
+                    System.out.println("        - cm (centímetros)");
+                    System.out.println("        - mm (dmilímetros)");
+                } while(opcion2==false);
+            }
+            
+        }
     }
     
 }
