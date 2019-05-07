3df0d19a411645704a4fa5f6bdc0237cb4d40d52
Cesáreo <cesareo.santiaguesrgmail.com>
2019-05-07 14:53:10 +0200

    Crear Clase Longitudes

---
+++ conversorUnidades/src/conversorunidades/Longitudes.java
@@ -0,0 +1,20 @@
+package conversorunidades;
+public class Longitudes {
+    /*Pasamos de unidad1 a unidad2*/
+    private String unidad1;
+    private String unidad2;
+    /*Cantidad que recibimos*/
+    private double cantidad1;
+    /*Cantidad convertida*/
+    private double cantidad2;
+
+    public Longitudes(String unidad1, String unidad2, double cantidad1) {
+        this.unidad1 = unidad1;
+        this.unidad2 = unidad2;
+        this.cantidad1 = cantidad1;
+    }
+    
+    /*Pasamos primero a metros y despues a la unidad que queremos*/
+    
+    
+}
