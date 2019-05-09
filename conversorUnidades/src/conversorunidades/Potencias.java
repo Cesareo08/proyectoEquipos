943e7cb3fbf99f5cb14ad2b21343245241eea25b
Brian <brianfblanco@gmail.com>
2019-05-09 11:23:59 +0200

    Potencias class done

---
+++ conversorUnidades/src/conversorunidades/Potencias.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversorunidades;
+
+/**
+ *
+ * @author dam130
+ */
+public class Potencias {
+    private double cantidad1;
+    private double cantidad2;
+    
+    
+    public Potencias(double c){
+        this.cantidad1=c;
+    }
+    public void KWaCV(){
+        this.cantidad2=this.cantidad1*1.34102;
+    }
+    public void CVaKW(){
+         this.cantidad2=this.cantidad1/1.34102;
+    }
+    public void KWaFtLb(){
+        this.cantidad2=this.cantidad1*737.562149;
+    }
+    public void FtLbaKW(){
+        this.cantidad2=this.cantidad1/737.562149;
+    }
+    
+    public double getCantidad2(){
+        return this.cantidad2;
+    }
+           
+
+
+}
